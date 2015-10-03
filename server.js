a875f01d24a53b8b33cb19b61c0635b068d78795
Michael Schwartz <mikethedj4@yahoo.com>
2015-10-03 05:09:10 -0500

    added node.js server

---
+++ server.js
@@ -0,0 +1,25 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require("http");
+var path = require("path");
+
+var express = require("express");
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname)));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
