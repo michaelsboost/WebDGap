30668560d823027afe2c51fb27972217cbee168c
Michael <MIchael@Michaels-MacBook.local>
2017-03-23 23:28:56 -0500

    fix video play on lockscreen
    by removing the video

--- www/js/webdgap.js
+++ www/js/webdgap.js
@@ -10,28 +10,11 @@ document.addEventListener("deviceready", function(){
       type : 'iframe', // Content type: image|inline|ajax|iframe|html (optional)
     });
 
-    // Inject YouTube API script
-    var tag = document.createElement('script');
-    tag.src = "//www.youtube.com/player_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    var player;
-    function onYouTubePlayerAPIReady(e) {
-      // create the global player from the specific iframe (#video)
-      player = new YT.Player($(".fancybox-iframe").attr("id"), {
-        events: {
-          // call this function when player is ready to use
-          'onReady': onPlayerReady
-        }
-      });
-    }
-
-    function onPlayerReady() {
+    $(document).on('onComplete.fb', function( e, instance, slide ) {
       document.addEventListener("pause", function(e) {
-        player.pauseVideo();
-      }, false);
-    }
+        $.fancybox.close();
+      });
+    });
   } else {
     alertify.alert("No internet connection detected! Can not export for Windows, Linux or Mac OS X!").set("basic", true);
   }
