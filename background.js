795d1a1bf1df72811c89b14db39175fdd925e8bc
Michael <MIchael@MichaelsMacBook.attlocal.net>
2017-05-06 15:25:25 -0500

    chrome extension's source code

---
+++ background.js
@@ -0,0 +1,18 @@
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create(
+    'index.html',
+    {
+      id: 'mainWindow',
+      innerBounds: {
+        'width': 800,
+        'height': 600
+      }
+    }
+  );
+});
